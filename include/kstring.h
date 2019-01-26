7b8cfcdf362c0c892f13a7ea2cb1ab5bcf633750
Krystian S <sdkrystian@gmail.com>
2019-01-25 19:31:43 -0500

    Fixed whitespace bug

--- include/kstring.h
+++ include/kstring.h
@@ -8,7 +8,7 @@ template<class, class = void>
 struct is_iterator : std::false_type { };
 
 template<class T>
-struct is_iterator <T, decltype(std::iterator_traits<T>::iterator_category(), void())> : std::true_type { };
+struct is_iterator<T, decltype(std::iterator_traits<T>::iterator_category(), void())> : std::true_type { };
 
 template<typename T>
 constexpr bool is_iterator_v = is_iterator<T>::value;
